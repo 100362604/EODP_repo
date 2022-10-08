439b4b38787e53c4de0bb71b266aa7692a18b354
100362604 <99724133+100362604@users.noreply.github.com>
2022-10-08 13:15:27 +0200

    test l1b

--- l1b/Test/testlL1b.py
+++ l1b/Test/testlL1b.py
@@ -2,19 +2,25 @@ from common.io.writeToa import readToa
 from config.globalConfig import globalConfig
 import matplotlib.pyplot as plt
 
-for band in globalConfig.bands:
+#for band in globalConfig.bands:
 
-    #my output
-    #toa_mine = readToa(directory, filename)
+    #my toa
+directory_mine = '/Users/luciamarssanchez/Documents/Earth_Observation/lib_out'
+toa_mine = readToa(directory_mine, 'l1b_toa_eq_VNIR-0.nc')
 
-    #lucia soto toa
-    #toa = readToa(directory, filename)
+    #toa lucia soto
+directory_ls = '/Users/luciamarssanchez/Documents/Earth_Observation/EODP_TER_2021/EODP-TS-L1B/output'
+toa_ls = readToa(directory_ls,'l1b_toa_eq_VNIR-0.nc')
 
-    #(toa_mine - toa_lucia)/toa_lucia tiene que ser menos que el valor que pone
+substraction = (toa_mine - toa_ls)
+print(substraction)
 
-    #max_rel_dif = np.max(np.abs(toa_mine -toa))
+#max and min value of the substraction
 
-    #print("Max rel dif", max_rel_dif, "band", band)
+print(max(map(max, substraction)))
+print(min(map(min, substraction)))
 
-    #plot
+
+
+
 
