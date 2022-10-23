5eccd78c954a688afd79908abc3c1f3bfe52a92f
100362604 <99724133+100362604@users.noreply.github.com>
2022-10-23 13:47:17 +0200

    test ISM

---
+++ ism/Test/test_ISM-0001.py
@@ -0,0 +1,28 @@
+from common.io.writeToa import readToa
+from config.globalConfig import globalConfig
+import numpy as np
+
+"""Check for all bands that the differences with respect to the output TOA (ism_toa_isrf) are <0.01% for at least
+ 3-sigma of the points."""
+
+directory_mine = '/Users/luciamarssanchez/Documents/Earth_Observation/lsm_out'
+directory_ls = '/Users/luciamarssanchez/Documents/Earth_Observation/EODP_TER_2021/EODP-TS-ISM/output'
+
+file = ['ism_toa_isrf_VNIR-0.nc','ism_toa_isrf_VNIR-1.nc','ism_toa_isrf_VNIR-2.nc','ism_toa_isrf_VNIR-3.nc']
+tolerance = 0.0001
+
+for i in range(len(file)):
+    toa_mine = readToa(directory_mine, file[i])
+    toa_ls = readToa(directory_ls, file[i])
+
+    substraction = np.zeros((toa_mine.shape[0], toa_mine.shape[1]))
+
+    for ii in range(toa_mine.shape[0]):
+        for jj in range(toa_mine.shape[1]):
+            substraction[ii,jj] = toa_mine[ii,jj] - toa_ls[ii,jj]
+
+if max(map(max,substraction)) <= tolerance:
+    print('Test 1: PASS')
+else:
+    print('Test 1: FAIL')
+
