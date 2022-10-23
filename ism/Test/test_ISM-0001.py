d76b5a497fb7cc87c01d78c4970c9937852300d3
100362604 <99724133+100362604@users.noreply.github.com>
2022-10-23 13:52:30 +0200

    test ISM optical

--- ism/Test/test_ISM-0001.py
+++ ism/Test/test_ISM-0001.py
@@ -8,21 +8,41 @@ import numpy as np
 directory_mine = '/Users/luciamarssanchez/Documents/Earth_Observation/lsm_out'
 directory_ls = '/Users/luciamarssanchez/Documents/Earth_Observation/EODP_TER_2021/EODP-TS-ISM/output'
 
-file = ['ism_toa_isrf_VNIR-0.nc','ism_toa_isrf_VNIR-1.nc','ism_toa_isrf_VNIR-2.nc','ism_toa_isrf_VNIR-3.nc']
+file_isrf = ['ism_toa_isrf_VNIR-0.nc','ism_toa_isrf_VNIR-1.nc','ism_toa_isrf_VNIR-2.nc','ism_toa_isrf_VNIR-3.nc']
 tolerance = 0.0001
 
-for i in range(len(file)):
-    toa_mine = readToa(directory_mine, file[i])
-    toa_ls = readToa(directory_ls, file[i])
+for i in range(len(file_isrf)):
+    toa_mine_isrf = readToa(directory_mine, file_isrf[i])
+    toa_ls_isrf = readToa(directory_ls, file_isrf[i])
 
-    substraction = np.zeros((toa_mine.shape[0], toa_mine.shape[1]))
+    substraction_isrf = np.zeros((toa_mine_isrf.shape[0], toa_mine_isrf.shape[1]))
 
-    for ii in range(toa_mine.shape[0]):
-        for jj in range(toa_mine.shape[1]):
-            substraction[ii,jj] = toa_mine[ii,jj] - toa_ls[ii,jj]
+    for ii in range(toa_mine_isrf.shape[0]):
+        for jj in range(toa_mine_isrf.shape[1]):
+            substraction_isrf[ii,jj] = toa_mine_isrf[ii,jj] - toa_ls_isrf[ii,jj]
 
-if max(map(max,substraction)) <= tolerance:
-    print('Test 1: PASS')
+if max(map(max,substraction_isrf)) <= tolerance:
+    print('Test 1 ISRF: PASS')
 else:
-    print('Test 1: FAIL')
+    print('Test 1 ISRF: FAIL')
 
+""" Check for all bands that the differences with respect to the output TOA (ism_toa_optical) 
+are <0.01% for at least 3-sigma of the points."""
+
+file_optical =['ism_toa_optical_VNIR-0.nc','ism_toa_optical_VNIR-1.nc','ism_toa_optical_VNIR-2.nc','ism_toa_optical_VNIR-3.nc']
+tolerance = 0.0001
+
+for i in range(len(file_optical)):
+    toa_mine_optical = readToa(directory_mine, file_optical[i])
+    toa_ls_optical = readToa(directory_ls, file_optical[i])
+
+    substraction_optical = np.zeros((toa_mine_optical.shape[0], toa_mine_optical.shape[1]))
+
+    for ii in range(toa_mine_optical.shape[0]):
+        for jj in range(toa_mine_optical.shape[1]):
+            substraction_optical[ii,jj] = toa_mine_optical[ii,jj] - toa_ls_optical[ii,jj]
+
+if max(map(max,substraction_optical)) <= tolerance:
+    print('Test 1 OPTICAL: PASS')
+else:
+    print('Test 1 OPTICAL: FAIL')
