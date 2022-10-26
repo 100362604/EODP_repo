49c7593d28b8ff37bc1d4fe2e8fc3090f6026ff1
100362604 <99724133+100362604@users.noreply.github.com>
2022-10-26 15:14:22 +0200

    test 2 ism

--- ism/Test/test_ISM-0002.py
+++ ism/Test/test_ISM-0002.py
@@ -8,23 +8,89 @@ import numpy as np
 directory_mine = '/Users/luciamarssanchez/Documents/Earth_Observation/lsm_out'
 directory_ls = '/Users/luciamarssanchez/Documents/Earth_Observation/EODP_TER_2021/EODP-TS-ISM/output'
 
-file = ['ism_toa_VNIR-0.nc','ism_toa_VNIR-1.nc','ism_toa_VNIR-2.nc','ism_toa_VNIR-3.nc']
+## Detection
+file_detection = ['ism_toa_detection_VNIR-0.nc','ism_toa_detection_VNIR-1.nc','ism_toa_detection_VNIR-2.nc','ism_toa_detection_VNIR-3.nc']
 tolerance = 0.0001
 
-for i in range(len(file)):
-    toa_mine = readToa(directory_mine, file[i])
-    toa_ls = readToa(directory_ls, file[i])
+for i in range(len(file_detection)):
+    toa_mine_detection = readToa(directory_mine, file_detection[i])
+    toa_ls_detection = readToa(directory_ls, file_detection[i])
 
-    substraction = np.zeros((toa_mine.shape[0], toa_mine.shape[1]))
+    substraction_detection = np.zeros((toa_mine_detection.shape[0], toa_mine_detection.shape[1]))
 
-    for ii in range(toa_mine.shape[0]):
-        for jj in range(toa_mine.shape[1]):
-            substraction[ii,jj] = toa_mine[ii,jj] - toa_ls[ii,jj]
+    for ii in range(toa_mine_detection.shape[0]):
+        for jj in range(toa_mine_detection.shape[1]):
+            substraction_detection[ii,jj] = toa_mine_detection[ii,jj] - toa_ls_detection[ii,jj]
 
 
-if max(map(max,substraction)) <= tolerance:
-    print('Test 1 ISRF: PASS')
+if max(map(max,substraction_detection)) <= tolerance:
+    print('Test Detection: PASS')
 else:
-    print('Test 1 ISRF: FAIL')
+    print('Test Detection: FAIL')
 
-print(max(map(max,substraction)))
+
+##### Ds
+file_ds = ['ism_toa_ds_VNIR-0.nc','ism_toa_ds_VNIR-1.nc','ism_toa_ds_VNIR-2.nc','ism_toa_ds_VNIR-3.nc']
+tolerance = 0.0001
+
+for i in range(len(file_ds)):
+    toa_mine_ds = readToa(directory_mine, file_ds[i])
+    toa_ls_ds = readToa(directory_ls, file_ds[i])
+
+    substraction_ds = np.zeros((toa_mine_ds.shape[0], toa_mine_ds.shape[1]))
+
+    for ii in range(toa_mine_ds.shape[0]):
+        for jj in range(toa_mine_ds.shape[1]):
+            substraction_ds[ii,jj] = toa_mine_ds[ii,jj] - toa_ls_ds[ii,jj]
+
+
+if max(map(max,substraction_ds)) <= tolerance:
+    print('Test Ds: PASS')
+else:
+    print('Test Ds: FAIL')
+
+
+##### e
+
+file_e = ['ism_toa_e_VNIR-0.nc','ism_toa_e_VNIR-1.nc','ism_toa_e_VNIR-2.nc','ism_toa_e_VNIR-3.nc']
+tolerance = 0.0001
+
+for i in range(len(file_detection)):
+    toa_mine_e = readToa(directory_mine, file_detection[i])
+    toa_ls_e = readToa(directory_ls, file_detection[i])
+
+    substraction_e = np.zeros((toa_mine_e.shape[0], toa_mine_e.shape[1]))
+
+    for ii in range(toa_mine_e.shape[0]):
+        for jj in range(toa_mine_e.shape[1]):
+            substraction_e[ii,jj] = toa_mine_e[ii,jj] - toa_ls_e[ii,jj]
+
+
+if max(map(max,substraction_e)) <= tolerance:
+    print('Test e: PASS')
+else:
+    print('Test e: FAIL')
+
+print(max(map(max,substraction_e)))
+
+#### PRNU
+
+file_PRNU = ['ism_toa_PRNU_VNIR-0.nc','ism_toa_PRNU_VNIR-1.nc','ism_toa_PRNU_VNIR-2.nc','ism_toa_PRNU_VNIR-3.nc']
+tolerance = 0.0001
+
+for i in range(len(file_PRNU)):
+    toa_mine_PRNU = readToa(directory_mine, file_PRNU[i])
+    toa_ls_PRNU = readToa(directory_ls, file_PRNU[i])
+
+    substraction_PRNU = np.zeros((toa_mine_PRNU.shape[0], toa_mine_PRNU.shape[1]))
+
+    for ii in range(toa_mine_PRNU.shape[0]):
+        for jj in range(toa_mine_PRNU.shape[1]):
+            substraction_PRNU[ii,jj] = toa_mine_PRNU[ii,jj] - toa_ls_PRNU[ii,jj]
+
+
+if max(map(max,substraction_PRNU)) <= tolerance:
+    print('Test PRNU: PASS')
+else:
+    print('Test PRNU: FAIL')
+
